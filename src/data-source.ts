3dee7ddb2a823b97674336c0b0ae80e2a79228d5
MAHAMAT Annour Mahamat <annour-mahamat.mahamat@niji.fr>
2025-04-09 16:41:43 +0200

    log in datasource file

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,7 +1,13 @@
 import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
+import { Logger } from '@nestjs/common';
+
+const logger = new Logger('DataSource');
+
+logger.log(`DATABASE_URL: ${process.env.DATABASE_URL}`);
 
 dotenv.config();
+
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
