5efb4f4c68cc67ade6673681109d7683621c0420
MAHAMAT Annour Mahamat <annour-mahamat.mahamat@niji.fr>
2025-04-09 16:16:10 +0200

    ajout url

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,6 +5,7 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
+  url: process.env.DATABASE_URL,
   entities: ['src/**/*.entity.ts'],
   migrations: ['src/migrations/*.ts'],
   synchronize: false,
